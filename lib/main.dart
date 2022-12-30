592b88782d69a35ae1e8a55cf0206c0f96ec5879
jeminblack <jeminthesiya101@gmail.com>
2022-12-30 18:56:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gst/GST.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: gst(),
+    ),
+  );
+}
